73d7f6d19cef70a35d65952f62799319216c331f
Kostas Kokkas <lowfideliter@hotmail.com>
2025-11-29 20:15:00 +0000

    Latest changes

--- Video_Stats.py
+++ Video_Stats.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 
 API_KEY = os.getenv("API_KEY")
 CHANNEL_HANDLE = "MrBeast"
+maxResults = 50
+
 
 load_dotenv(dotenv_path="./.env")
 
@@ -24,13 +26,17 @@ def get_playlist_id():
 
         channel_items = data['items'][0]
         channel_playlistid = channel_items['contentDetails']['relatedPlaylists']['uploads']
-        #print(channel_playlistid)
+        print(channel_playlistid)
 
         return channel_playlistid
 
     except requests.exceptions.RequestException as e:
         raise e
 
+base_url = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxResults}&playlistId=UUX60Q3DkcsbYNE6H8uQQuVA&key{API_KEY}"
+
+
+playlistID=get_playlist_id()
 
 if __name__ == "__main__": #name is for pushing the code directly, in this case the VS and not importing it. Main is the variable for running the script.
     get_playlist_id()
