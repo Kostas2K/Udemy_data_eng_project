cc6be412ce6939acbdbe543913f3518966bbc8b9
Kostas Kokkas <lowfideliter@hotmail.com>
2025-11-30 17:19:49 +0000

    Adding the latest changes

--- Video_Stats.py
+++ Video_Stats.py
@@ -33,11 +33,44 @@ def get_playlist_id():
     except requests.exceptions.RequestException as e:
         raise e
 
-base_url = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxResults}&playlistId=UUX60Q3DkcsbYNE6H8uQQuVA&key{API_KEY}"
 
 
-playlistID=get_playlist_id()
+
+#playlistID=get_playlist_id()
+
+def get_video_ids(playlistId):
+    video_ids = []
+    pageToken = None
+    #https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults=1&pageToken=EAAaHlBUOkNBRWlFRVUxTlVFNVFqZzJRVEF4UXprMU1rRQ&playlistId=UUX6OQ3DkcsbYNE6H8uQQuVA&key=[YOUR_API_KEY]
+    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxResults}&playlistId=UUX6OQ3DkcsbYNE6H8uQQuVA&key{API_KEY}"
+
+    try:
+        while True:
+            url = base_url
+            if pageToken:
+                url += f"&pageToken={pageToken}"
+
+            response = requests.get(url)
+
+            response.raise_for_status()
+
+            data = response.json()
+
+            for item in data.get('items',[]):    
+                video_id = item['contentDetails']['videoId']
+                video_ids.append(video_id)
+
+            pageToken = data.get('nextPageToken')
+            if not pageToken:
+                break
+
+        #print(video_ids)
+        return video_ids
+
+    except requests.exceptions.RequestException as e:
+        raise e
+
 
 if __name__ == "__main__": #name is for pushing the code directly, in this case the VS and not importing it. Main is the variable for running the script.
-    get_playlist_id()
-
+    playlistID = get_playlist_id()
+    print(get_video_ids(playlistID))
